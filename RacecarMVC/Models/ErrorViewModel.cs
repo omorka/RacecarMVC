fcbdf736365743d7d54acb0eaafe05bcc3a887b4
omorka <36473263+omorka@users.noreply.github.com>
2022-09-01 17:45:19 -0400

    Initial commit

---
+++ RacecarMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RacecarMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RacecarMVC/Models/Palindrome.cs
@@ -0,0 +1,10 @@
+﻿namespace RacecarMVC.Models
+{
+    public class Palindrome
+    {
+        public string InputWord { get; set; }
+        public string RevWord { get; set; }
+        public bool IsPalindrome { get; set; }
+        public string Message { get; set; }
+    }
+}
